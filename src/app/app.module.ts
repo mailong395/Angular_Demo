adaae15bda1a5f015f4a04e13b1422080d893d56
MaiNgocLongY2001 <long.mai@beesolutionsgroup.com>
2023-10-17 15:18:33 +0700

    clear code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -38,10 +38,3 @@ import { ShippingComponent } from './shipping/shipping.component';
   ]
 })
 export class AppModule { }
-
-
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at https://angular.io/license
-*/
